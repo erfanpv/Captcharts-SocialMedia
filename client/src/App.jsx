80107084d5da2c979bc596d39e29a586a47060cb
erfanpv <erfanpv786@gmail.com>
2024-11-11 18:00:30 +0530

    fix(App.jsx): update imports and structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./componetns/ui/Navbar";
+import HomePage from "./pages/HomePage";
+import FeedPage from "./pages/FeedPage";
+
 function App() {
   return (
-    <>
-      <h1 >Social Media App</h1>
-    </>
+    <div className="bg-[#F7F7F7]">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/feed" element={<FeedPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
